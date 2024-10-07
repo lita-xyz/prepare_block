eb9db74599321ee3fc43674a1e949c6b3a4418d0
Lewis Revill <lewis1revill@gmail.com>
2024-10-07 13:50:03 +0100

    TEMP: Debug minimal input

--- src/utils/init.rs
+++ src/utils/init.rs
@@ -16,14 +16,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use super::db::RemoteDb;
+use alloy_primitives::Bytes;
 use alloy_provider::{Provider, ProviderBuilder};
 use alloy_rpc_types::{BlockId, BlockTransactions, BlockTransactionsKind};
 use anyhow::Result;
 use async_trait::async_trait;
 use reth_valida::primitives::alloy2reth::IntoReth;
-use reth_valida::primitives::mpt::proofs_to_tries;
-use reth_valida::primitives::processor::EvmProcessor;
 use reth_valida::primitives::ValidaRethInput;
 use std::collections::HashSet;
 use url::Url;
@@ -52,83 +50,9 @@ impl ValidaRethInputInitializer for ValidaRethInput {
             .get_block(BlockId::from(block_number), BlockTransactionsKind::Full)
             .await?
             .unwrap();
-
-        // Intiialize the db.
-        let provider_db = RemoteDb::new(provider, parent_header.number);
-
-        // Create the input.
-        let txs = match block.transactions {
-            BlockTransactions::Full(txs) => txs.into_iter().map(|tx| tx.into_reth()).collect(),
-            _ => unreachable!(),
-        };
-        let withdrawals = block
-            .withdrawals
-            .unwrap()
-            .into_iter()
-            .map(|w| w.into_reth())
-            .collect();
         let input = ValidaRethInput {
-            beneficiary: block.header.miner,
-            gas_limit: block.header.gas_limit.try_into().unwrap(),
             timestamp: block.header.timestamp,
-            extra_data: block.header.extra_data,
-            mix_hash: block.header.mix_hash.unwrap(),
-            transactions: txs,
-            withdrawals,
-            parent_state_trie: Default::default(),
-            parent_storage: Default::default(),
-            contracts: Default::default(),
-            parent_header: parent_header.into_reth(),
-            ancestor_headers: Default::default(),
         };
-
-        let mut executor = EvmProcessor::<RemoteDb> {
-            input: input.clone(),
-            db: Some(provider_db),
-            header: None,
-        };
-        executor.initialize();
-        let mut executor = tokio::task::spawn_blocking(move || {
-            executor.execute();
-            executor
-        })
-        .await?;
-
-        // Get the proofs and ancestor headers.
-        let mut provider_db = executor.db.take().unwrap();
-        let (parent_proofs, proofs, ancestor_headers, provider_db) =
-            tokio::task::spawn_blocking(move || {
-                let parent_proofs = provider_db.fetch_initial_storage_proofs().unwrap();
-                let proofs = provider_db.fetch_latest_storage_proofs().unwrap();
-                let ancestor_headers = provider_db.fetch_ancestor_headers().unwrap();
-                (parent_proofs, proofs, ancestor_headers, provider_db)
-            })
-            .await?;
-
-        // Get the contracts from the initial db.
-        let mut contracts = HashSet::new();
-        let initial_db = provider_db.initial_db;
-        for account in initial_db.accounts.values() {
-            let code = &account.info.code;
-            if let Some(code) = code {
-                let bytes = code.bytes();
-                contracts.insert(bytes.clone());
-            }
-        }
-
-        // Construct the state trie and storage from the proofs.
-        let (state_trie, storage) =
-            proofs_to_tries(input.parent_header.state_root, parent_proofs, proofs)?;
-
-        // Create the block builder input
-        let input = ValidaRethInput {
-            parent_state_trie: state_trie,
-            parent_storage: storage,
-            contracts: contracts.iter().cloned().collect(),
-            ancestor_headers,
-            ..input
-        };
-
         // DONE!
 
         Ok(input)
